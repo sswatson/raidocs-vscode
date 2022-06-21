8191486289dfc2de7e1307f03b88ba96fe5fd8ee
Samuel S. Watson <samuel.s.watson@gmail.com>
2022-06-21 16:11:27 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# raidocs README
+
+The raidocs extension provides tools to support documentation authors at RelationalAI.
+
+## Snippets
+
+To use the extension, open the `raidocs-next` folder as your workspace in VS Code and then open a Markdown file that you want to edit. Type one of the following:
+
+- `rel`
+- `table`
+- `alert`
+- `image`
+
+Press `control+space` with the cursor immediately following the last character. Select the raidocs option in the resulting menu, and a snippet will be inserted. Press tab to go from one placeholder to the next.
+
+## Images
+
+As an alternative to the `image` snippet, you can run a command that copies an image to the correct directory and inserts the appropriate `ImgFig` code into your Markdown file.  Do `command+shift+P` to open the command palette, and start typing "insert image". Select the option called "raidocs: Insert Image" when it appears. Use the file dialog that opens to choose the desired image.
+
+## References
+
+You can use the extension to create references to sections of other articles. Do `command+shift+P` to open the command palette, and start typing "insert reference". Select the option called "raidocs: Insert Reference" when it appears. You'll get a list of articles on the site, which you can filter by typing search characters. When you select the article you want to link, a list of headers within that article will appear. When you select the relevant section header, the path for that section is inserted.
---
+++ src/extension.ts
@@ -0,0 +1,125 @@
+import * as vscode from 'vscode';
+
+// @ts-ignore
+async function* walk(dir: vscode.Uri) {
+	for await (const [fileName, fileType] of await vscode.workspace.fs.readDirectory(dir)) {
+		const entry = vscode.Uri.joinPath(dir, fileName);
+		if (fileType === vscode.FileType.Directory) {
+			yield* walk(entry);
+		} else if (fileType === vscode.FileType.File) {
+			yield entry;
+		}
+	}
+}
+
+export function activate(context: vscode.ExtensionContext) {
+
+	const refCommand = vscode.commands.registerCommand(
+		'raidocs.insertReference', 
+		async () => {
+			const folders = vscode.workspace.workspaceFolders;
+			if (!folders || !folders.length) {
+				vscode.window.showInformationMessage('no folder open');
+				return;
+			}
+			const rootDir = folders[0].uri;
+			const pagesDir = vscode.Uri.joinPath(rootDir, "site/pages");
+			let articles = [];
+			for await (let file of walk(pagesDir)) {
+				const fileName = file.toString();
+				if (fileName.endsWith(".md")) {
+					articles.push({
+						label:
+						fileName.split('site/pages')[1].replace(/.md$/g, '').slice(1),
+						file
+					});
+				}
+			}
+			const selectedArticle = await vscode.window.showQuickPick(articles);
+			if (selectedArticle) {
+				const contentBytes = await vscode.workspace.fs.readFile(
+					selectedArticle.file
+				);
+				const contents = Buffer.from(contentBytes).toString();
+				const headers = [];
+				for (let line of contents.split('\n')) {
+					if (line.match(/^#+ /)) { // match header lines
+						const ref = line
+							.replace(/^#+ /, '')
+							.toLowerCase()
+							.replace(/[^a-zA-Z ]/g, '')
+							.replace(/ /g, '-');
+						headers.push({
+							label: line,
+							ref
+						});
+					}
+				}
+				const selectedHeader = await vscode.window.showQuickPick(headers);
+				if (selectedHeader) {
+					const editor = vscode.window.activeTextEditor;
+					if (editor) {
+						editor.edit(edit => edit.insert(
+							editor.selection.active,
+							'/' + selectedArticle.label + '/#' + selectedHeader.ref
+						));
+					}
+				}
+			}
+		});
+		const imageCommand = vscode.commands.registerCommand(
+			'raidocs.insertImage', 
+			async () => {
+				const folders = vscode.workspace.workspaceFolders;
+				if (!folders || !folders.length) {
+					vscode.window.showInformationMessage('no folder open');
+					return;
+				}
+				if (vscode?.window?.activeTextEditor) {
+					const imgDir = vscode
+						.window
+						.activeTextEditor
+						.document
+						.fileName
+						.replace("pages", "public")
+						.replace(/\.md$/,'');
+					const selectedUris = await vscode.window.showOpenDialog({
+						canSelectMany: false,
+						openLabel: "Select Image",
+						filters: {
+							'Images': ['png', 'jpg', 'jpeg', 'svg']
+						},
+					});
+					if (selectedUris) {
+						const imgPath = selectedUris[0].toString();
+						const imgName = imgPath.split('/').pop();
+						if (imgName) {
+							vscode.workspace.fs.copy(
+								selectedUris[0], 
+								vscode.Uri.joinPath(vscode.Uri.file(imgDir), imgName)
+							);
+							if (vscode?.window?.activeTextEditor) {
+								const img = `<ImgFig
+  src="${imgDir.split('site/public')[1] + '/' + imgName}"
+  width="90%"
+  alt="${imgName.replace(/\.[a-z]*$/, '')}"
+/>`;
+								vscode.window.activeTextEditor.edit(edit => {
+									if (vscode?.window?.activeTextEditor) {
+										edit.insert(
+											vscode.window.activeTextEditor.selection.active,
+											img
+										)
+									}
+								});
+							}
+						}
+					}
+				}
+			});		
+	context.subscriptions.push(refCommand);
+	context.subscriptions.push(imageCommand);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}

97201c16dc5c91aae1ffd6fbbb78c5d092fa3da1
Samuel S. Watson <samuel.s.watson@gmail.com>
2023-01-30 23:23:15 -0500

    gloss document functionality

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,8 @@ import insertReference from './lib/insertReference';
 import runCodeCells from './lib/runCodeCells';
 import startServer from './lib/startServer';
 import checkReferences from './lib/checkReferences';
+import glossSelection from './lib/glossSelection';
+import glossDocument from './lib/glossDocument';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -31,12 +33,22 @@ export function activate(context: vscode.ExtensionContext) {
 		'raidocs.checkReferences',
 		checkReferences
 	);
+	const glossSelectionCommand = vscode.commands.registerCommand(
+		'raidocs.glossSelection',
+		glossSelection
+	);
+	const glossDocumentCommand = vscode.commands.registerCommand(
+		'raidocs.glossDocument',
+		glossDocument
+	);
 	context.subscriptions.push(refCommand);
 	context.subscriptions.push(imageCommand);
 	context.subscriptions.push(runCodeCellsCommand);
 	context.subscriptions.push(runAllCodeCellsCommand);
 	context.subscriptions.push(startServerCommand);
 	context.subscriptions.push(checkReferencesCommand);
+	context.subscriptions.push(glossSelectionCommand);
+	context.subscriptions.push(glossDocumentCommand);
 }
 
 // this method is called when your extension is deactivated

84c2f9eadd2acb1ad78a756eaf180b511a2b324e
Samuel S. Watson <samuel.s.watson@gmail.com>
2023-02-03 11:41:09 -0500

    update node version for terminal commands

--- src/lib/checkReferences.ts
+++ src/lib/checkReferences.ts
@@ -2,7 +2,7 @@ import { getTerminal } from './utils';
 
 const checkReferences = async () => {
 	const { terminal } = getTerminal('raidocs references');
-	terminal.sendText('nvm use 14.19.0');
+	terminal.sendText('nvm use 18.13.0');
   terminal.sendText('node tools/check-references.js');
 	terminal.show();
 }
--- src/lib/startServer.ts
+++ src/lib/startServer.ts
@@ -3,7 +3,7 @@ import { getTerminal } from './utils';
 
 const startServer = async () => {
 	const { terminal } = getTerminal('raidocs server');
-	terminal.sendText('nvm use 14.19.0');
+	terminal.sendText('nvm use 18.13.0');
 	if (process.platform === 'darwin') {
 		terminal.sendText('(sleep 5 && open http://localhost:3000) &');
 	}
